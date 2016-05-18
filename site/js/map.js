f15e2c3be6ab4fc50f31535f016baabd72f2a932
Liljana Dimovska <lddimovska@gmail.com>
2016-05-18 14:21:05 +0200

    Fix za mapata

--- site/js/map.js
+++ site/js/map.js
@@ -9,7 +9,7 @@
                     zoom: 11,
 
                     // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(40.6700, -73.9400), // New York
+                    center: new google.maps.LatLng(42.0041, 21.4095), // FINKI
 
                     // How you would like to style the map. 
                     // This is where you would paste any style found on Snazzy Maps.
